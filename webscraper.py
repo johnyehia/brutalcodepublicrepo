5634f36954191c0bdacb9bb5fc3ebf699dc51c14
SivamshIndukuri <107054653+SivamshIndukuri@users.noreply.github.com>
2024-02-24 18:23:21 -0500

    NewCODE
    now with methods

--- webscraper.py
+++ webscraper.py
@@ -18,51 +18,53 @@ class Question:
         
 
 
+class webscraper:
 
-LeetUser = input("Input your LeetCode Username ")
-my_url = "https://leetcode-api-faisalshohag.vercel.app/" + LeetUser
+    def createArray():
+        LeetUser = input("Input your LeetCode Username ")
+        my_url = "https://leetcode-api-faisalshohag.vercel.app/" + LeetUser
 
-client = uReq(my_url)
+        client = uReq(my_url)
 
-page_html = client.read()
+        page_html = client.read()
 
-page_html
+        page_html
 
-page_soup = soup(page_html, features= "html.parser")
+        page_soup = soup(page_html, features= "html.parser")
 
-page_soup.getText
+        page_soup.getText
 
-text = page_soup.getText() + ""
+        text = page_soup.getText() + ""
 
-completedNum= int(text[15:text.index(",")])
-print(completedNum)
-print(completedNum)
-finish=0
-questionArray = list()
-while completedNum > finish:
-
-
-    AllQuestion = ""
-
-    indexStart = text.index("\"title\":")+9
-    text = text[indexStart:]
-    indexEnd = text.index(",")-1
-    name=text[0:indexEnd]
-    text = text[indexEnd:]
+        completedNum= int(text[15:text.index(",")])
     
+        finish=0
+        questionArray=list()
+        while completedNum > finish:
+
+
+            AllQuestion = ""
+
+            indexStart = text.index("\"title\":")+9
+            text = text[indexStart:]
+            indexEnd = text.index(",")-1
+            name=text[0:indexEnd]
+            text = text[indexEnd:]
+
     
-    try:
-       AllQuestion.index(name)
+            try:
+                AllQuestion.index(name)
        
      
-    except:
-       finish = finish+1
-       indexStartTime = text.index("timestamp")+12
-       text = text[indexStart:]
-       endtext = text.index(",")-1
-       date = text[0:indexEnd]
-       AllQuestion=AllQuestion+name
-       questionArray.append(Question(name,date))
+            except:
+                finish = finish+1
+                indexStartTime = text.index("timestamp")+12
+                text = text[indexStart:]
+                endtext = text.index(",")-1
+                date = text[0:indexEnd]
+                AllQuestion=AllQuestion+name
+                questionArray.append(Question(name,date))
+            return questionArray
        
   
 
