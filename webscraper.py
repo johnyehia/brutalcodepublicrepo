3c9f9d1b2d5f0d90b4dcf4444129788d28198500
Yogesh Sampathkumar <108630212+yoge1212@users.noreply.github.com>
2024-02-24 20:35:09 -0500

    offical webscrape

--- webscraper.py
+++ webscraper.py
@@ -1,7 +1,7 @@
 import datetime
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
-import pandas as pd
+
 
 
 
@@ -18,72 +18,62 @@ class Question:
         
 
 
+class Webscraper:
 
-LeetUser = input("Input your LeetCode Username ")
-my_url = "https://leetcode-api-faisalshohag.vercel.app/" + LeetUser
+    def createArray():
+        LeetUser = input("Input your LeetCode Username ")
+        my_url = "https://leetcode-api-faisalshohag.vercel.app/" + LeetUser
 
-client = uReq(my_url)
+        client = uReq(my_url)
 
-page_html = client.read()
+        page_html = client.read()
 
-page_html
+        page_html
 
-page_soup = soup(page_html, features= "html.parser")
+        page_soup = soup(page_html, features= "html.parser")
 
-page_soup.getText
+        page_soup.getText
 
-text = page_soup.getText() + ""
+        text = page_soup.getText() + ""
 
-completedNum= int(text[15:text.index(",")])
-print(completedNum)
-print(completedNum)
-
-questionArray = list()
-while text.index("\"title\":")+9  != text.rindex("\"title\":")+9:
-
-    #print("times")
+        completedNum= int(text[15:text.index(",")])
     
-    indexStart = text.index("\"title\":")+9
-    text = text[indexStart:]
-    indexEnd = text.index(",")-1
-    name=text[0:indexEnd]
-    text = text[indexEnd:]
-    
-    indexStartTime = text.index("timestamp")+12
-    text = text[indexStartTime:]
-    endtext = text.index(",")-1
-    date = text[0:endtext]
-    text = text[endtext:]
-
-    indexStartAccepted = text.index("statusDisplay")+16
-    text = text[indexStartAccepted:]
-    endtext = text.index(",")-1
-    value = text[0:endtext]
-    
-    text = text[endtext:]
-    
-    Add = True
-    for i in questionArray:
-        if(i.returnName()==name):
-            Add = False
-        if(value != "Accepted"):
-            Add = False
-
-    if Add == True:
         
-        questionArray.append(Question(name,date))
-
-
-for j in questionArray:
-    print(j.returnName())
-        
+        questionArray=list()
+        while text.index("\"title\":")+9  != text.rindex("\"title\":")+9:
 
     
+            indexStart = text.index("\"title\":")+9
+            text = text[indexStart:]
+            indexEnd = text.index(",")-1
+            name=text[0:indexEnd]
+            text = text[indexEnd:]
+            
+            indexStartTime = text.index("timestamp")+12
+            text = text[indexStartTime:]
+            endtext = text.index(",")-1
+            date = text[0:endtext]
+            text = text[endtext:]
 
+            indexStartAccepted = text.index("statusDisplay")+16
+            text = text[indexStartAccepted:]
+            endtext = text.index(",")-1
+            value = text[0:endtext]
+            
+            text = text[endtext:]
+            
+            Add = True
+            for i in questionArray:
+                if(i.returnName()==name):
+                    Add = False
+                if(value != "Accepted"):
+                    Add = False
+
+            if Add == True:
+                
+                questionArray.append(Question(name,date))
+        return questionArray
        
-
-
-
   
 
 
